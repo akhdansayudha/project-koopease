8760933d5e1ac41980fe36b8ab80170f5f5d14d2
fahrezyyy <naufalfahrezy@student.telkomuniversity.ac.id>
2025-12-05 20:30:03 +0700

    update kecil-kecilan

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -23,22 +23,6 @@
     </style>
 </head>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/692a65a5c860c2197de63359/1jb6pq41m';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
-
 <body class="text-gray-800">
 
     <!-- Your existing content here -->
